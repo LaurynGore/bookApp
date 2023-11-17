ad8a057d3599880b7b35abedca0cfa78b41a44e7
Lauryn Gore <lauryngore@gmail.com>
2023-11-17 15:31:51 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.0"
+    application
 }
 
 group = "ie.setu"
@@ -11,12 +13,18 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation ("org.slf4j:slf4j-simple:1.7.36")
+    implementation ("io.github.microutils:kotlin-logging:2.1.23")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-kotlin {
-    jvmToolchain(8)
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
 }
+
+//application {
+//    mainClass.set("MainKt")
+//}
