077bdba8abc68dcc963f5a5b220c6a55e6cd7c57
Lauryn Gore <lauryngore@gmail.com>
2023-11-17 15:31:28 +0000

    Setting up main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,2 +1,65 @@
-class main {
+import controllers.BookApi
+import models.Book
+import models.Chapters
+import utils.ScannerInput
+import mu.KotlinLogging
+import kotlin.math.ln
+import utils.ScannerInput.readNextInt
+import utils.ScannerInput.readNextLine
+
+private val logger = KotlinLogging.logger {}
+private val book = BookApi()
+fun main(){
+    runMenu()
+}
+
+fun mainMenu(): Int {
+    return ScannerInput.readNextInt(""" 
+         > ----------------------------------
+         > |        Book APP         |
+         > ----------------------------------
+         > | Book MENU                      |
+         > |   1) Add a book                |
+         > |   2) List all books            |
+         > |   3) Update a book             |
+         > |   4) Delete a book             |
+         > ----------------------------------
+         > |   0) Exit                      |
+         > ----------------------------------
+         > ==>> """.trimMargin(">"))
+}
+
+fun runMenu(){
+    do {
+        val option = mainMenu()
+        when (option) {
+            1  -> addBook()
+            2  -> listBooks()
+            3  -> updateBook()
+            4  -> deleteBook()
+            0  -> exitApp()
+            else -> println("Invalid option entered: ${option}")
+        }
+    } while (true)
+}
+
+fun addBook(){
+    return println("Add book")
+}
+
+fun listBooks(){
+    return println("List books")
+}
+
+fun updateBook(){
+    return println("Update a book")
+}
+
+fun deleteBook(){
+    return println("Delete a book")
+}
+
+fun exitApp(){
+    logger.info { "exitApp() function invoked" }
+    System.exit(0)
 }
