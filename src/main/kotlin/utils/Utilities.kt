7791d4085f51172f00fc14feaf3be2da7ea3203f
Lauryn Gore <lauryngore@gmail.com>
2023-12-03 20:01:30 +0000

    update Utlities.kt

--- src/main/kotlin/utils/Utilities.kt
+++ src/main/kotlin/utils/Utilities.kt
@@ -1,6 +1,21 @@
 package utils
-import java.lang.NumberFormatException
-import java.util.*
 
-class Utilities {
+import models.Chapters
+import models.Book
+
+object Utilities {
+
+    // NOTE: JvmStatic annotation means that the methods are static i.e. we can call them over the class
+    //      name; we don't have to create an object of Utilities to use them.
+
+    @JvmStatic
+    fun formatListString(notesToFormat: List<Book>): String =
+        notesToFormat
+            .joinToString(separator = "\n") { book ->  "$book" }
+
+    @JvmStatic
+    fun formatSetString(itemsToFormat: Set<Chapters>): String =
+        itemsToFormat
+            .joinToString(separator = "\n") { chapter ->  "\t$chapter" }
+
 }
